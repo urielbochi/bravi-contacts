87a547294c262cb79d2c010e71e5bf308d42b7f0
Uriel <urielbochi@gmail.com>
2022-09-24 10:17:11 -0300

    feat: dev dependencies

--- README.md
+++
@@ -1,1 +0,0 @@
-# bravi-contacts
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require("express");
+
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cors())
+app.use('/', ); 
+
+app.listen(3000, () => console.log("Conectado"))
