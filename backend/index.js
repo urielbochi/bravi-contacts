26ea9de49bdbca421154207cab17044a693a7d9d
Uriel <urielbochi@gmail.com>
2022-09-25 10:47:49 -0300

    feat: change index routes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 const express = require("express");
-const contactRoutes = require('./routes/contactRoutes')
+const contactRoutes = require('./routes/contact')
 
-const cors = require('cors')
+const cors = require('cors');
+const root = require('./routes/root');
 
 const app = express();
 
 app.use(express.json())
 
 app.use(cors())
-app.use('/', contactRoutes); 
+app.use('/', root); 
 
 app.listen(3000, () => console.log("Conectado"))
