a26af130d5c8961fa1718fa5a17549e31d418392
Uriel <urielbochi@gmail.com>
2022-09-27 00:39:58 -0300

    feat: build front-end

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Contacts from './Components/Contacts';
+import MainRoutes from './Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <MainRoutes/>
+    </div>
+  );
+}
+
+export default App;
