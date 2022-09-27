07aeaff7d102183cda26ebe98cd2927875bf593d
Uriel <urielbochi@gmail.com>
2022-09-27 02:11:51 -0300

    feat: contact page

--- frontend/src/Components/ContactPage/index.tsx
+++ frontend/src/Components/ContactPage/index.tsx
@@ -1,16 +1,35 @@
-function ContactPage() {
+import { useContext } from "react";
+import { ContactContext } from "../../Context/Event";
+import { deleteContact } from "../../Services";
 
+function ContactPage({contact, contactId}:any) {
 
+    const { contactList, setContactList, setContactClicked, setEditPageClicked }:any = useContext(ContactContext);
+
+    
+  function deleteFrontContact() {
+    const contactListAfterRemove = contactList.filter(
+      (item: any) => item.id !== contactId
+    );
+    setContactList(contactListAfterRemove);
+  }
+
+  function deleteContactCalls() {
+    deleteContact(contactId);
+    deleteFrontContact();
+    setContactClicked(false);
+  }
+  
     return (
         <div className='split left medium-grade'>
             <div className='close__contact' onClick={() => setContactClicked(false)}>X</div>
             <div className='centered'>
-                <h1 className='userinfo__padding'>{colectContact.name}</h1>
-                <h3 className='userinfo__padding'>{colectContact.phone}</h3>
-                <h3 className='userinfo__padding'>{colectContact.email}</h3>
-                <h3 className='userinfo__padding'>{colectContact.whatsapp}</h3>
+                <h1 className='userinfo__padding'>{contact.name}</h1>
+                <h3 className='userinfo__padding'>{contact.phone}</h3>
+                <h3 className='userinfo__padding'>{contact.email}</h3>
+                <h3 className='userinfo__padding'>{contact.whatsapp}</h3>
                 <button className='delete' onClick={() => deleteContactCalls()}>Delete contact</button>
-                <button className='delete' onClick={() => setEditTable(true)}>Edit contact</button>
+                <button className='delete' onClick={() => setEditPageClicked(true)}>Edit contact</button>
             </div>
         </div>
     )
