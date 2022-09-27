924c2de61480ef36b5e3c37da4de030aabb9d035
Uriel <urielbochi@gmail.com>
2022-09-27 00:38:19 -0300

    feat: get contact by id

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -4,10 +4,12 @@ const create = require("../Controller/Contact/create")
 const remove = require("../Controller/Contact/delete")
 const getAll = require("../Controller/Contact/getAll");
 const getAllById = require("../Controller/Contact/getAllById");
+const getById = require("../Controller/Contact/getById");
 const update = require("../Controller/Contact/update");
 
 
 router.get("/", getAll)
+router.get("/item/:id", getById)
 router.get("/:id", getAllById)
 router.post("/", create);
 router.delete("/:id",  remove);
